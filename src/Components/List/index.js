3fcede1926c23ad2313a1d937aef8b0ee5632bd1
Nandan Kumar <nkumar101@sapient.com>
2018-09-21 16:20:32 +0530

    added minor css fixes

--- src/Components/List/index.js
+++ src/Components/List/index.js
@@ -13,7 +13,8 @@ class List extends Component {
             <h3 className="News-points">{news.points} ★★★</h3>
             <div className="News-content">
                 <div className="News-title">{news.title} <span className="News-domain">{news.domain}</span> </div>
-                <div className="News-details">By {news.user} <span className="time-ago">{news.time_ago}</span> | <span className="comments">{news.comments}</span></div>
+                <div className="News-details">By <a href="">{news.user}</a> | <span className="time-ago">{news.time_ago}</span></div>
+                <div className="comments"><a href="#">{news.comments_count} Comments</a></div>
             </div>
         </div>
         ):''
